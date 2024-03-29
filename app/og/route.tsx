f11b2c7574e80d7d2384930ac8917bf2d16dce7e
Javier Toja Alamancos <jtojaa@MacBook-Air-de-Javier.local>
2024-03-29 18:22:32 +0100

    try without nested divs

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -27,16 +27,7 @@ export async function GET(request: Request) {
             fontSize: 32,
             fontWeight: 600,
           }}
-        >
-          <svg
-            width="75"
-            viewBox="0 0 75 65"
-            fill="#000"
-            style={{ margin: '0 75px' }}
-          >
-            <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
-          </svg>
-          <div style={{ marginTop: 40, display: 'flex' }}>Hello, ${joke_hash}</div>
+        >Hello, ${joke_hash}
         </div>
         ),
         {
